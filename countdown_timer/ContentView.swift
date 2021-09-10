e153c3cccef624405d9850de497c8cfa1a1760fc
ayumu021431 <ayumu_skywalker@icloud.com>
2021-09-11 00:27:55 +0900

    Initial Commit

---
+++ countdown_timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  countdown_timer
+//
+//  Created by asora on 2021/09/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ countdown_timer/countdown_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdown_timerApp.swift
+//  countdown_timer
+//
+//  Created by asora on 2021/09/11.
+//
+
+import SwiftUI
+
+@main
+struct countdown_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
